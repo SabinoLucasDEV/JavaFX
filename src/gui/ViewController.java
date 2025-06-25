88058c39d638d49bf4ceccff0d864fda3b6b9c2a
N00bM4T3R <sabinolbn1810@gmail.com>
2025-06-25 10:20:07 -0300

    ComboBox

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,54 +1,54 @@
 package gui;
 
+
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import gui.util.Alerts;
-import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import model.entity.Person;
+
 
 public class ViewController  implements Initializable{
 	
 	@FXML
-	private TextField txtNumber1;
+	private ComboBox<Person> comboPerson;
 	
 	@FXML
-	private TextField txtNumber2;
+	private Button btAll;
 	
 	@FXML
-	private Label labelResult;
-	
-	
-	@FXML
-	private Button btSum;
-	
-	@FXML
-	public void onbtSumAction() {
-		try {
-		Locale.setDefault(Locale.US);
-		Double number1 = Double.parseDouble(txtNumber1.getText());
-		Double number2 = Double.parseDouble(txtNumber2.getText());
-		Double result = number1 + number2;
-		labelResult.setText(String.format("%.2f", result));
+	private void onBtAll() {
+		for (Person person : comboPerson.getItems()) {
+			System.out.println(person);
 		}
-		catch (NumberFormatException e) {
-			Alerts.showAlert("NumberFormatException", null, e.getMessage(), AlertType.ERROR);
-		}
+	}
+	
+	private ObservableList<Person> obsList;
+	@FXML
+	private void onComboAction() {
+		Person person = comboPerson.getSelectionModel().getSelectedItem();
+		System.out.println(person);
 		
-		
-		}
+	}
+	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-	 Constraints.setTextFieldDouble(txtNumber1);
-	 Constraints.setTextFieldDouble(txtNumber2);
+	List<Person> list = new ArrayList<Person>();
+	list.add(new Person(1, "maria", "maria@gmail.com"));
+	list.add(new Person(2, "alex", "alex@gmail.com"));
+	list.add(new Person(3, "bob", "bob@gmail.com"));
+	
+	obsList = FXCollections.observableArrayList(list);
+	comboPerson.setItems(obsList);
+	
 		
 	}
 	
